0205dc5a80ece8f2d9a676674f6e6b70ee8bfb82
Vanshikaxxa <vanshikhashri@gmail.com>
2025-03-05 23:59:54 +0530

    navbar modi-fied

--- public/components/Navbar.tsx
+++ public/components/Navbar.tsx
@@ -2,10 +2,23 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="w-screen h-auto bg-[#55562F] ">
-      <div>
-        <image></image>
+    /*main nav */
+    <nav className="w-screen bg-[#55562F] text-[#FFFDD0] flex justify-between items-center p-3">
+      <div className="pl-3 flex justify-start">
+        <img
+          src="/images/logo/Logo3.webp"
+          className="w-16 n-12 rounded-4xl -m-1 "
+          alt="main-logo"
+        />
       </div>
+      <div className="flex list-none gap-10">
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Donate</li>
+        <li>Volunteer</li>
+        <li>Charity</li>
+      </div>
+      <button className="pl-20 pr-3">Donate Money</button>
     </nav>
   );
 };
