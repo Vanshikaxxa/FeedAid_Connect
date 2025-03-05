2dbe530c80a3144040d3605efc5fba537d0c2347
Vanshikaxxa <vanshikhashri@gmail.com>
2025-03-05 22:47:53 +0530

    navbar added

---
+++ public/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-screen h-auto bg-[#55562F] ">
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;
