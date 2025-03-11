4b7a13eb581ba58750a4651b7fc57073a452a4e6
Vanshikaxxa <vanshikhashri@gmail.com>
2025-03-11 12:14:16 +0530

    testonials upadated

--- public/components/Testimonials.tsx
+++ public/components/Testimonials.tsx
@@ -10,30 +10,57 @@ const Testimonials = () => {
       <h4 className="text-center text-[#0b2a33] font-semibold text-4xl md:my-12">
         Reviews from partners
       </h4>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 justify-center mx-auto max-w-6xl py-2 px-4">
-        <div className="bg-[#ffffff] p-8 rounded-2xl shadow-md">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-center mx-auto max-w-6xl py-4">
+        <div className="bg-[#ffffff] p-10 rounded-2xl shadow-md">
           <h5 className="text-2xl font-semibold">Lankenau Medical Center</h5>
-          <p className="mt-4 text-gray-600">
+          <p className="mt-4 text-xl text-[#163d4a]">
             "Through the generosity of Food Connect, we are able to deliver
             fresh food and produce to address the food insecurity and chronic
             disease management needs of our patients, who would otherwise lack
             access to fresh food in their daily lives."
           </p>
+          <div className="flex items-center pt-12 space-x-4">
+            <img
+              src="\images\testimonials\testimonials 1.jpg"
+              alt="Maureen Krouse"
+              className="w-12 h-12 rounded-full"
+            />
+            <div>
+              <p className="font-semibold text-[#163d4a]">Maureen Krouse</p>
+              <p className="text-[#163d4a]">
+                Manager at Community Health and Equity
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="bg-[#ffffff] p-8 rounded-2xl shadow-md">
+
+        <div className="bg-[#ffffff] p-10 rounded-2xl shadow-md">
           <h5 className="text-2xl font-semibold">SF New Deal</h5>
-          <p className="mt-4 text-gray-600">
+          <p className="mt-4 text-xl text-[#163d4a]">
             "It's been a joy working with Food Connect! Their dispatch, drivers,
             and support team are responsive, kind & thorough, and their services
             have enabled our team to manage & minimize food waste in our daily
             operations."
           </p>
+          <div className="flex items-center pt-12 space-x-4">
+            <img
+              src="\images\testimonials\testimonials 3.jpg"
+              alt="Sarah Morris"
+              className="w-12 h-12 rounded-full"
+            />
+            <div>
+              <p className="font-semibold text-[#163d4a]">Sarah Morris</p>
+              <p className="text-[#163d4a]">
+                Director of Client Services & Call Center
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="bg-[#ffffff] p-8 rounded-2xl shadow-md">
+        <div className="bg-[#ffffff] p-10 rounded-2xl shadow-md">
           <h5 className="text-2xl font-semibold">
             Children’s Hospital of Philadelphia
           </h5>
-          <p className="mt-4 text-gray-600">
+          <p className="mt-4 text-xl text-[#163d4a]">
             “Food Connect is the reason CHOP’s Food Pharmacy was able to not
             only maintain Food Pharmacy operations during the onset of COVID,
             but helped us expand our programming allowing us to reach more
@@ -44,10 +71,23 @@ const Testimonials = () => {
             flexibility and personalized attention has been the key to the
             growth of CHOP’s Food Pharmacy.”
           </p>
+          <div className="flex items-center pt-12 space-x-4">
+            <img
+              src="\images\testimonials\testimonials 2.jpg"
+              alt="Abbe Stern"
+              className="w-12 h-12 rounded-full"
+            />
+            <div>
+              <p className="font-semibold text-[#163d4a]">Abbe Stern</p>
+              <p className="text-[#163d4a]">
+                Food Pharmacy Manager, Healthier Together
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="bg-[#ffffff] p-8 rounded-2xl shadow-md">
+        <div className="bg-[#ffffff] p-12 rounded-2xl shadow-md">
           <h5 className="text-2xl font-semibold">18 Reasons</h5>
-          <p className="mt-4 text-gray-600">
+          <p className="mt-4 text-xl text-[#163d4a]">
             "Working with Food Connect has enabled us to deliver healthy
             ingredients to diabetic patients all over the San Francisco Bay
             Area. Food Connect is communicative and collaborative and works hard
@@ -58,13 +98,24 @@ const Testimonials = () => {
             enabling our community with the confidence and creativity to buy,
             cook, and eat good food every day! "
           </p>
+          <div className="flex items-center pt-12 space-x-4">
+            <img
+              src="\images\testimonials\testimonials 4.jpg"
+              alt="Francesca Boulton"
+              className="w-12 h-12 rounded-full"
+            />
+            <div>
+              <p className="font-semibold text-[#163d4a]">Francesca Boulton</p>
+              <p className="text-[#163d4a]">Community Programs Director</p>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="bg-[#fcf8f7] flex flex-col min-screen min-h-[500px] w-full px-6 py-12">
-        <h4 className="text-center text-[#163d4a] text-4xl md:my-12">
-          Our Program Design Plan makes building food programs easier than ever
-          before
+      <div className="bg-[#fcf8f7] flex flex-col min-screen min-h-[500px] w-full px-10 py-12">
+        <h4 className="text-center font-semibold px-8 text-[#163d4a] text-4xl md:my-10 leading-snug md:w-3/4 mx-auto">
+          Our Program Design Plan makes building <br /> food programs easier
+          than ever before
         </h4>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 m-6 justify-items-center">
