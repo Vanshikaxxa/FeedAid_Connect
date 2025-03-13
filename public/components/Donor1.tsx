033d0506b5f4fbd310ccc5d52e64ba7073eb0610
Flashl3opard <sheoreyyash@gmail.com>
2025-03-13 20:26:46 +0530

    Added Donor page

--- public/components/Donor1.tsx
+++ public/components/Donor1.tsx
@@ -2,28 +2,37 @@ import React from "react";
 
 const Donor1 = () => {
   return (
-    <div className="bg-gradient-to-r from-blue-200 to-teal-200 flex flex-col md:flex-row items-center justify-center p-5 md:p-10 rounded-lg shadow-lg h-auto md:h-96 my-15">
-      <img
-        src="/images/donor/don1.webp"
-        alt="Donor Image"
-        className="w-full h-auto md:h-full rounded-lg  mb-5 md:mb-0 shadow-md"
-      />
-      <div className="flex flex-col items-start md:items-center md:ml-5">
-        <h1 className="text-base md:text-lg lg:text-xl m-5 font-semibold text-gray-800 text-center md:text-left">
-          The quality, wholesome prepared food donated from restaurants is a
-          welcomed addition in the total effort to eliminate hunger . The food
-          is deeply appreciated by the food rescue programs committed to serving
-          those who experience hunger. Our Donor Partners understand that, when
-          prepared food is involved, a small amount at a time can rapidly build
-          up to make a big difference.
-        </h1>
-        <div className="flex justify-center w-full">
-          <button className="border border-blue-600 bg-blue-600 text-white p-3 rounded-xl hover:bg-blue-700 transition duration-300 ease-in-out shadow-md">
-            Become a Donor
+    <>
+      <section className="flex flex-row justify-center items-center relative h-screen">
+        <img
+          src="/images/donor/wallp2.png"
+          alt="Food Donation Background"
+          className="bg-cover w-full h-full"
+        />
+
+        <div className="absolute left-0 top-0 bg-none backdrop-blur-md p-10 rounded-lg shadow-2xl h-full w-full max-w-md flex flex-col justify-center">
+          <h1 className="text-5xl font-bold mb-4 my-35 text-green-950 text-center">
+            Donate Food
+          </h1>
+          <p className="mb-6 text-green-900 text-center text-xl font-light leading-relaxed">
+            The quality, wholesome prepared food donated from restaurants is a
+            welcomed addition in the total effort to eliminate hunger. The food
+            is deeply appreciated by the food rescue programs committed to
+            serving those who experience hunger. Our Donor Partners understand
+            that, when prepared food is involved, a small amount at a time can
+            rapidly build up to make a big difference.
+          </p>
+          <button className="bg-blue-600 text-white py-3 px-6 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out shadow-md">
+            Request a pickup
           </button>
+          <div className="mt-4 text-center">
+            <a href="/signup" className="text-blue-700 hover:underline">
+              Haven't signed up yet??
+            </a>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
--- public/components/Donor3.tsx
+++ public/components/Donor3.tsx
@@ -1,12 +1,74 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
+import { FaRegMap } from "react-icons/fa6";
+import { MdDeliveryDining } from "react-icons/md";
+import { GiImpactPoint } from "react-icons/gi";
+import { IoPeople } from "react-icons/io5";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Donor3 = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+    });
+  }, []);
+
   return (
-    <>
-      <div className="">
-        <h1>Donate for poor peoples treatment and medicine.</h1>
+    <div>
+      <div
+        className="bg-[#fcf8f7] flex flex-col min-h-[500px] w-full px-10 py-12"
+        data-aos="fade-up"
+      >
+        <h4 className="text-center font-semibold px-8 text-[#163d4a] text-4xl md:my-10 leading-snug md:w-3/4 mx-auto">
+          Our Program Design Plan makes building <br /> food programs easier
+          than ever before
+        </h4>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 m-6 justify-items-center">
+          <div className="flex flex-col items-center text-center w-full max-w-sm">
+            <div className="p-6 bg-[#ffcfbf] rounded-full">
+              <FaRegMap className="text-8xl text-[#f68b6a]" />
+            </div>
+            <h3 className="mt-4 font-semibold text-[#163d4a]">
+              Map your program needs
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-center text-center w-full max-w-sm">
+            <div className="p-6 bg-[#ffcfbf] rounded-full">
+              <MdDeliveryDining className="text-8xl text-[#f68b6a]" />
+            </div>
+            <h3 className="mt-4 font-semibold text-[#163d4a]">
+              Set up deliveries to transport food
+            </h3>
+          </div>
+
+          <div className="flex flex-col items-center text-center w-full max-w-sm">
+            <div className="p-6 bg-[#ffcfbf] rounded-full">
+              <GiImpactPoint className="text-8xl text-[#f68b6a]" />
+            </div>
+            <h3 className="mt-4 font-semibold text-[#163d4a]">Track impact</h3>
+          </div>
+
+          <div className="flex flex-col items-center text-center w-full max-w-sm">
+            <div className="p-6 bg-[#ffcfbf] rounded-full">
+              <IoPeople className="text-8xl text-[#f68b6a]" />
+            </div>
+            <h3 className="mt-4 font-semibold text-[#163d4a]">
+              Engage with customers and improve outcomes
+            </h3>
+          </div>
+        </div>
+        <div className="flex justify-center py-6 pd-8">
+          <button className="bg-[#26634e] text-2xl hover:bg-[#283c43] text-white font-semibold px-6 py-4 rounded-sm shadow-lg transition-colors duration-300 cursor-pointer">
+            Learn more about programs
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
