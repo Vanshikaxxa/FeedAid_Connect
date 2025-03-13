631acf17d5558a3c0c8a4b6f016a68ac15222eab
Flashl3opard <sheoreyyash@gmail.com>
2025-03-13 20:29:43 +0530

    Changed the font

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -2,19 +2,22 @@ import Donor1 from "@/public/components/Donor1";
 import Donor2 from "@/public/components/Donor2";
 import Donor3 from "@/public/components/Donor3";
 import Footer from "@/public/components/Footer";
-
 import Navbar from "@/public/components/Navbar";
 import React from "react";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const page = () => {
   return (
     <>
-      <Navbar />
-
-      <Donor1 />
-      <Donor2 />
-      <Donor3 />
-      <Footer />
+      <div className={inter.className}>
+        <Navbar />
+        <Donor1 />
+        <Donor2 />
+        <Donor3 />
+        <Footer />
+      </div>
     </>
   );
 };
