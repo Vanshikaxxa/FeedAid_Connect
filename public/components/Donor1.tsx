f47962a0ceec54e330a0f3a4f5bb4b07d0b19be2
Flashl3opard <sheoreyyash@gmail.com>
2025-03-13 21:11:17 +0530

    Changes in donate page

--- public/components/Donor1.tsx
+++ public/components/Donor1.tsx
@@ -10,11 +10,11 @@ const Donor1 = () => {
           className="bg-cover w-full h-full"
         />
 
-        <div className="absolute left-0 top-0 bg-none p-10 rounded-lg h-full w-full max-w-md flex flex-col justify-center translate-y-10">
-          <h1 className="text-5xl font-bold mb-4 text-green-950 text-center">
+        <div className="absolute left-0 top-0 bg-none p-10 rounded-lg h-full w-3/4 flex flex-col justify-center translate-y-10">
+          <h1 className="text-6xl font-bold mb-4 text-green-950 text-left">
             Donate Food
           </h1>
-          <p className="mb-6 text-black text-center text-xl font-medium leading-relaxed">
+          <p className="mb-6 text-black text-left text-2xl font-medium leading-relaxed">
             The quality, wholesome prepared food donated from restaurants is a
             welcomed addition in the total effort to eliminate hunger. The food
             is deeply appreciated by the food rescue programs committed to
@@ -22,12 +22,12 @@ const Donor1 = () => {
             that, when prepared food is involved, a small amount at a time can
             rapidly build up to make a big difference.
           </p>
-          {/* Flex container for buttons */}
-          <div className="flex justify-between mt-4">
-            <button className="bg-green-700 text-white py-3 px-4 rounded-lg hover:bg-green-800 transition duration-300 ease-in-out shadow-md">
+
+          <div className="flex items-start mt-4 space-x-4">
+            <button className="bg-green-700 text-white py-3 px-6 rounded-lg hover:bg-green-800 transition duration-300 ease-in-out shadow-md">
               Request a pickup
             </button>
-            <button className="bg-blue-600 text-white py-3 px-4 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out shadow-md">
+            <button className="bg-blue-600 text-white py-3 px-6 rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out shadow-md">
               Become a donor
             </button>
           </div>
